378f9ae30b2e971210a5a4eff05c900f42b55c60
Max Fan <root@max.fan>
2021-09-24 17:49:16 -0500

    Add stuff necessary for reproducing sept. 20th runs

---
+++ reproduce-sept20.sh
@@ -0,0 +1,23 @@
+python chains.py TX 50000 sep20 38 
+python chains.py TX 50000 sep20 31 
+python chains.py TX 50000 sep20 150
+
+python chains.py OH 50000 sep20 15  --pop-col TOTPOP
+python chains.py OH 50000 sep20 33  --pop-col TOTPOP
+python chains.py OH 50000 sep20 99  --pop-col TOTPOP
+
+python chains.py WI 50000 sep20 8 
+python chains.py WI 50000 sep20 33
+python chains.py WI 50000 sep20 99 
+
+python chains.py MD 50000 sep20 8 
+python chains.py MD 50000 sep20 47 
+# double-check no third?
+
+python chains.py IN 50000 sep20 9  --pop-col TOTPOP
+python chains.py IN 50000 sep20 50 --pop-col TOTPOP
+python chains.py IN 50000 sep20 100 --pop-col TOTPOP
+
+python chains.py MN 50000 sep20 8 
+python chains.py MN 50000 sep20 67
+python chains.py MN 50000 sep20 134
